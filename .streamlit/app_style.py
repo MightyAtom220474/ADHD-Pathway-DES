cf2ba3897dab496c3d350d988d71472627fd5f3e
MightyAtom220474 <heath.mcdonald@lscft.nhs.uk>
2024-11-21 14:00:12 +0000

    CSS: style css removed for testing as not working

--- .streamlit/app_style.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-   
-def global_page_style(file_path):  
-    with open(file_path) as f:
-        css = f.read()
-    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
