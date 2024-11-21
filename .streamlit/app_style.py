435d2f5c438f48538580b55e46f792bef6095d0a
MightyAtom220474 <heath.mcdonald@lscft.nhs.uk>
2024-11-21 07:56:02 +0000

    File Changed: app_style pointed to new css folder

--- .streamlit/app_style.py
+++ .streamlit/app_style.py
@@ -1,6 +1,6 @@
 import streamlit as st
    
 def global_page_style():  
-    with open('assets/style.css') as f:
+    with open('assets/CSS/style.css') as f:
         css = f.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
